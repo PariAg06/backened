99827eae27c56314a84469c4eea7a2bd2dc84c06
PariAg06 <124270057+PariAg06@users.noreply.github.com>
2024-11-18 19:51:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//<-----------------Server Creation---------------------------->
+require('dotenv').config()
+
+
+
+
+
+const express=require('express');
+const app=express();
+const port=4000;
+app.get('/',(req,res)=>{
+    res.send('hiteshdotcom');
+    
+})
+app.get('/twitter',(req,res)=>{
+    res.send('Hello World')
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1>Please login at chai aur code')
+})
+app.get('/youtube',(req,res)=>{
+    res.send('chai aur code')
+})
+app.listen(process.env.PORT,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
